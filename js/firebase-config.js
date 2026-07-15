19b96e48a4473aea5827757249fd482114640bab
primetoolslabs <primetoolslabs@gmail.com>
2026-07-15 08:02:48 -0300

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAQUj8f-Q6RB89XbhKZd69JXm9fth2tGg8",
+  authDomain: "team-f78cd.firebaseapp.com",
+  projectId: "team-f78cd",
+  storageBucket: "team-f78cd.firebasestorage.app",
+  messagingSenderId: "742988887447",
+  appId: "1:742988887447:web:6440bf438bd13e3bc200d2"
+};
+
+export const OWNER_EMAIL = "pimentelsolution1@gmail.com";
+export const FIREBASE_SDK_VERSION = "12.15.0";
