2788b448508efbf905b9f256b4c4e5571b65f31b
Primetools Labs <primetoolslabs@gmail.com>
2026-08-11 08:46:17 -0300

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAQUj8f-Q6RB89XbhKZd69JXm9fth2tGg8",
-  authDomain: "team-f78cd.firebaseapp.com",
-  projectId: "team-f78cd",
-  storageBucket: "team-f78cd.firebasestorage.app",
-  messagingSenderId: "742988887447",
-  appId: "1:742988887447:web:6440bf438bd13e3bc200d2"
-};
-export const FIREBASE_VERSION = "12.15.0";
